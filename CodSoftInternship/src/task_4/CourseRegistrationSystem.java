a2600b4338602fad2d7125c225fb7a5c40a59525
Prachi Kadam <147628245+Prachi-K11@users.noreply.github.com>
2023-11-07 22:59:05 -0800

    Update CourseRegistrationSystem.java

--- CodSoftInternship/src/task_4/CourseRegistrationSystem.java
+++ CodSoftInternship/src/task_4/CourseRegistrationSystem.java
@@ -3,20 +3,16 @@ package task_4;
 public class CourseRegistrationSystem {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Course mathCourse = new Course("MATH101", "Introduction to Mathematics", "Basic math course", 30);
         Course physicsCourse = new Course("PHYS101", "Physics Fundamentals", "Introductory physics", 25);
 
-        // Create students
         Student student1 = new Student(1, "Prachi");
         Student student2 = new Student(2, "punam");
-
-        // Register students for courses
+		
         mathCourse.registerStudent(student1);
         physicsCourse.registerStudent(student1);
         physicsCourse.registerStudent(student2);
-
-        // Display course information
+		
         System.out.println("Course Information:");
         System.out.println(mathCourse);
         System.out.println("\nStudent Information:");
